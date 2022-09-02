c967ab5eda164b09ba06ec57b77a7dccd814ffb8
Hurjui Andrei-Tiberiu <94405226+hurjui-tiberiu@users.noreply.github.com>
2022-09-02 17:54:18 +0300

    Feature/implement candidate controller and crud operations (#2)
    * implement-project-arhitecture
    
    * add-candidate-entity
    
    * feature/implement-candidate-controller-and-crud-operations

--- Voting-System/Program.cs
+++ Voting-System/Program.cs
@@ -1,6 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using Voting_System.Application.Interfaces;
+using Voting_System.Application.Services;
+using Voting_System.Infrastructure.Interfaces;
+using Voting_System.Infrastructure.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Services.AddTransient<ICandidateRepository, CandidateRepository>();
+builder.Services.AddTransient<ICandidateService, CandidateService>();
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
