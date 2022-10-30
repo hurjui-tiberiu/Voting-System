e67b6df907bc414219168db67d920400fdda284d
Hurjui Andrei-Tiberiu <94405226+hurjui-tiberiu@users.noreply.github.com>
2022-10-30 11:46:48 +0200

    Feature/implement redis caching (#14)
    * Develop (#11)
    
    * create-develop-branch
    
    * implement-project-arhitecture
    
    * implement-project-arhitecture
    
    * Implement project arhitecture (#1)
    
    * implement-project-arhitecture
    
    * add-candidate-entity
    
    * feature/implement-candidate-controller-and-crud-operations
    
    * Feature/implement candidate controller and crud operations (#2)
    
    * implement-project-arhitecture
    
    * add-candidate-entity
    
    * feature/implement-candidate-controller-and-crud-operations
    
    * implement-user-controller-and-crud-operations
    
    * Implement user controller and crud operations (#3)
    
    * implement-project-arhitecture
    
    * implement-project-arhitecture
    
    * add-candidate-entity
    
    * feature/implement-candidate-controller-and-crud-operations
    
    * Revert "Implement user controller and crud operations (#3)" (#4)
    
    This reverts commit 928f1a21a58357f5e88fbd4711ca57b20dfdd7e2.
    
    * implement-automapper
    
    * implement-entity-configurations-and-initial-migration
    
    * create-develop-branch
    
    * Solve conflicts
    
    * solve conflicts
    
    * feature/implement-jwt-authentication (#8)
    
    * Feature/implement fluent validation (#9)
    
    * feature/implement-jwt-authentication
    
    * feature/implement-fluent-validation
    
    * commit
    
    * implement-password-encryption
    
    * Create pages.yml
    
    * Delete .github/workflows directory
    
    * implement-redis-caching

---
+++ Voting-System/Extensions/DistributedCacheExtension.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Extensions.Caching.Distributed;
+using System.Text.Json;
+
+namespace Voting_System.Extensions
+{
+    public static class DistributedCacheExtension
+    {
+        public static async Task SetRecordAsync<T>(this IDistributedCache cache,
+            string recordId,
+            T data,
+            TimeSpan? absoluteExpireTime = null,
+            TimeSpan? unusedExpireTime = null
+            )
+        {
+            var options = new DistributedCacheEntryOptions();
+
+            options.AbsoluteExpirationRelativeToNow = absoluteExpireTime ?? TimeSpan.FromMinutes(10);
+            options.SlidingExpiration = unusedExpireTime ?? TimeSpan.FromMinutes(3);
+
+            var jsonData = JsonSerializer.Serialize(data);
+            await cache.SetStringAsync(recordId, jsonData, options);
+        }
+
+        public static async Task<T> GetRecordAsync<T>(this IDistributedCache cache, string recordId)
+        {
+            var jsonData = await cache.GetStringAsync(recordId);
+
+            if(jsonData is null)
+            {
+                return default(T)!;
+            }
+
+            return JsonSerializer.Deserialize<T>(jsonData)!;
+        }
+    }
+}
